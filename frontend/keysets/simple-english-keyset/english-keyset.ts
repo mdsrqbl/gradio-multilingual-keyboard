cc4c953ad7e6a4a146140450e6e0e653b3066ebb
Telha Bilal <telhabb@gmail.com>
2024-08-12 19:14:22 +0500

    Move to-be-shared functions between different languages to utils

--- frontend/keysets/simple-english-keyset/english-keyset.ts
+++ frontend/keysets/simple-english-keyset/english-keyset.ts
@@ -1,26 +1,8 @@
 import { IKeyboard } from "../../ikeyboard";
 import { Key, KeySet } from "../keyset";
+import { getSimpleKeyboardKeySpan, getMouseDownFunction, isSpecialKey } from "../utils";
 import { shiftedKeysLayout, unshiftedKeysLayout } from "./simple-english-keys-layout";
-import { charToCodesMapping } from "../../char-codes";
 
-
-function getModifiedValueOnKeyPress(curValue: string, key_str: string, states: object): string {
-
-    const shift = Boolean(states["shift"]);
-
-    if (key_str.length === 1) {
-        curValue += shift ? key_str.toUpperCase() : key_str.toLowerCase();
-    } else if (key_str === "Space") {
-        curValue += " ";
-    } else if (key_str === "Delete") {
-        curValue = curValue.slice(0, -1);
-    } else if (key_str === "Enter") {
-        curValue += "\n";
-    }
-
-    return curValue;
-
-}
 
 
 export class EnglishKeySet implements KeySet {
@@ -32,15 +14,25 @@ export class EnglishKeySet implements KeySet {
 
     constructor() {
 
-        this.unShiftedKeys = unshiftedKeysLayout.map((row) => {
+        this.unShiftedKeys = this.getKeyListsFromLayout(unshiftedKeysLayout);
+        this.shiftedKeys = this.getKeyListsFromLayout(shiftedKeysLayout);
+
+    }
+
+    private getKeyListsFromLayout(layout: string[][]): Key[][] {
+
+        let keyLists: Key[][] = layout.map((row) => {
             let keys: Key[] = [];
             row.forEach((key_str) => {
-                let span = this.getKeySpan(key_str);
-                let is_special = this.isSpecialKey(key_str);
-                let onDownFunction = this.getKeyDownFunction(key_str);
-                let onUpFunction = this.getKeyUpFunction(key_str);
-                let onMouseDownFunction = this.getMouseDownFunction(key_str);
-                let onMouseUpFunction = this.getMouseUpFunction(key_str);
+                let span = getSimpleKeyboardKeySpan(key_str);
+                let is_special = isSpecialKey(key_str);
+
+                let onMouseDownFunction = getMouseDownFunction(key_str, this);
+
+                // TODO implement later as needed.
+                let onDownFunction = () => {};
+                let onUpFunction = () => {};
+                let onMouseUpFunction = () => {};
 
                 keys.push(
                     new Key(
@@ -60,120 +52,7 @@ export class EnglishKeySet implements KeySet {
             return keys;
         });
 
-        this.shiftedKeys = shiftedKeysLayout.map((row) => {
-            let keys: Key[] = [];
-            row.forEach((key_str) => {
-                let span = this.getKeySpan(key_str);
-                let is_special = this.isSpecialKey(key_str);
-
-                let onDownFunction = this.getKeyDownFunction(key_str);
-                let onUpFunction = this.getKeyUpFunction(key_str);
-                let onMouseDownFunction = this.getMouseDownFunction(key_str);
-                let onMouseUpFunction = this.getMouseUpFunction(key_str);
-
-                keys.push(
-                    new Key(
-                        "",
-                        key_str,
-                        "",
-                        1,
-                        is_special,
-                        span,
-                        onDownFunction,
-                        onUpFunction,
-                        onMouseDownFunction,
-                        onMouseUpFunction,
-                    ),
-                );
-            });
-            return keys;
-        });
-    }
-
-    private getKeyDownFunction(key_str: string) {
-        return (e: KeyboardEvent) => { console.log(key_str + " Key Down") };
-    }
-
-    private getKeyUpFunction(key_str: string) {
-        return (e: KeyboardEvent) => { console.log(key_str + " Key Up") };
-    }
-
-    private getMouseDownFunction(key_str: string) {
-
-        let customMouseDownFn = () => {
-
-            if (key_str.toLowerCase() in ["shift", "ctrl", "alt"]) {
-                let curKeyState = Boolean(this.containingKeyboard.states[key_str.toLowerCase()]);
-                this.containingKeyboard.states[key_str.toLowerCase()] = !curKeyState;
-            }
-            else {
-
-                if (this.containingKeyboard.targetElement instanceof HTMLInputElement ||
-                    this.containingKeyboard.targetElement instanceof HTMLTextAreaElement) {
-
-                    let curValue = this.containingKeyboard.targetElement.value;
-                    let modifiedValue = getModifiedValueOnKeyPress(curValue, key_str, this.containingKeyboard.states);
-                    this.containingKeyboard.targetElement.value = modifiedValue;
-
-                }
-                else if (typeof (this.containingKeyboard.targetElement) === "string") {
-
-                    let curValue = this.containingKeyboard.targetElement;
-                    let modifiedValue = getModifiedValueOnKeyPress(curValue, key_str, this.containingKeyboard.states);
-                    this.containingKeyboard.targetElement = modifiedValue;
-                }
-            }
-
-            if (this.containingKeyboard.targetElement instanceof HTMLElement) {
-                this.containingKeyboard.targetElement.dispatchEvent(new Event('input'));  // notify value update
-                this.containingKeyboard.targetElement.dispatchEvent(new KeyboardEvent('keydown', {
-                    key: key_str,
-                    code: charToCodesMapping[key_str]['code'],
-                    keyCode: charToCodesMapping[key_str]['keyCode'],  // for backward compatibility
-                    ctrlKey: Boolean(this.containingKeyboard.states['ctrl']),
-                    shiftKey: Boolean(this.containingKeyboard.states['shift']),
-                    altKey: Boolean(this.containingKeyboard.states['alt']),
-                }));
-            }
-        };
-
-        return () => {
-            customMouseDownFn();
-        };
-    }
-
-    private getMouseUpFunction(key_str: string) {
-        return (e: MouseEvent) => { };
-    }
-
-    private getKeySpan(key: string): number {
-        return key === "Space"
-            ? 14
-            : key === "Shift"
-                ? 5
-                : key === "Delete" ||
-                    key === "Enter" ||
-                    key === "Ctrl" ||
-                    key === "Alt" ||
-                    key === "Caps"
-                    ? 4
-                    : key === "Tab" || key === "\\"
-                        ? 3
-                        : 2;
-    }
-
-    private isSpecialKey(key: string): boolean {
-        return [
-            "Delete",
-            "Enter",
-            "Shift",
-            "Caps",
-            "Tab",
-            "Ctrl",
-            "Alt",
-            "Win",
-            "Space",
-        ].includes(key);
+        return keyLists;
     }
 
     isLanguageRTL(): boolean {
---
+++ frontend/keysets/utils.ts
@@ -0,0 +1,96 @@
+import { charToCodesMapping } from "../char-codes";
+import { KeySet } from "./keyset";
+
+
+export function getSimpleKeyboardKeySpan(key: string): number {
+    return key === "Space"
+        ? 14
+        : key === "Shift"
+            ? 5
+            : key === "Delete" ||
+                key === "Enter" ||
+                key === "Ctrl" ||
+                key === "Alt" ||
+                key === "Caps"
+                ? 4
+                : key === "Tab" || key === "\\"
+                    ? 3
+                    : 2;
+}
+
+
+export function isSpecialKey(key: string): boolean {
+    return [
+        "Delete",
+        "Enter",
+        "Shift",
+        "Caps",
+        "Tab",
+        "Ctrl",
+        "Alt",
+        "Win",
+        "Space",
+    ].includes(key);
+}
+
+
+function getModifiedValueOnKeyPress(curValue: string, key_str: string, states: object): string {
+
+    const shift = Boolean(states["shift"]);
+
+    if (key_str.length === 1) {
+        curValue += shift ? key_str.toUpperCase() : key_str.toLowerCase();
+    } else if (key_str === "Space") {
+        curValue += " ";
+    } else if (key_str === "Delete") {
+        curValue = curValue.slice(0, -1);
+    } else if (key_str === "Enter") {
+        curValue += "\n";
+    }
+
+    return curValue;
+
+}
+
+
+export function getMouseDownFunction(key_str: string, callingKeyset: KeySet) {
+
+    let customMouseDownFn = () => {
+
+        if (key_str.toLowerCase() in ["shift", "ctrl", "alt"]) {
+            let curKeyState = Boolean(callingKeyset.containingKeyboard.states[key_str.toLowerCase()]);
+            callingKeyset.containingKeyboard.states[key_str.toLowerCase()] = !curKeyState;
+        }
+        else {
+
+            if (callingKeyset.containingKeyboard.targetElement instanceof HTMLInputElement ||
+                callingKeyset.containingKeyboard.targetElement instanceof HTMLTextAreaElement) {
+
+                let curValue = callingKeyset.containingKeyboard.targetElement.value;
+                let modifiedValue = getModifiedValueOnKeyPress(curValue, key_str, callingKeyset.containingKeyboard.states);
+                callingKeyset.containingKeyboard.targetElement.value = modifiedValue;
+
+            }
+            else if (typeof (callingKeyset.containingKeyboard.targetElement) === "string") {
+
+                let curValue = callingKeyset.containingKeyboard.targetElement;
+                let modifiedValue = getModifiedValueOnKeyPress(curValue, key_str, callingKeyset.containingKeyboard.states);
+                callingKeyset.containingKeyboard.targetElement = modifiedValue;
+            }
+        }
+
+        if (callingKeyset.containingKeyboard.targetElement instanceof HTMLElement) {
+            callingKeyset.containingKeyboard.targetElement.dispatchEvent(new Event('input'));  // notify value update
+            callingKeyset.containingKeyboard.targetElement.dispatchEvent(new KeyboardEvent('keydown', {
+                key: key_str,
+                code: charToCodesMapping[key_str]['code'],
+                keyCode: charToCodesMapping[key_str]['keyCode'],  // for backward compatibility
+                ctrlKey: Boolean(callingKeyset.containingKeyboard.states['ctrl']),
+                shiftKey: Boolean(callingKeyset.containingKeyboard.states['shift']),
+                altKey: Boolean(callingKeyset.containingKeyboard.states['alt']),
+            }));
+        }
+    };
+
+    return customMouseDownFn;
+}
